08dce46f784534e45995f345f65f8bc627710fc7
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-12 16:00:47 -0700

    fixed slider - needed import react-slick css files to make slider work correctly. Also mapped through languages to minimize coding and separated all slider details into the SliderDetails.js file

---
+++ client/src/components/LanguageSlider/SliderDetails.js
@@ -0,0 +1,30 @@
+export const languages = [
+    "HTML",
+    "Javascript",
+    "CSS",
+    "Jquery",
+    "React",
+    "Bootstrap",
+    "Node.js",
+    "Sequelize",
+    "Passport",
+    "MySQL",
+    "GraphQL",
+    "Apollo",
+    "Mongoose",
+    "Handlebars",
+    "EJS",
+]
+
+export const sliderSettings = {
+    slidesToScroll: 1,
+    slidesPerRow: 1,
+    vertical: true,
+    rows: 3,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    speed: 750,
+    autoplaySpeed: 2500
+};
--- client/src/components/LanguageSlider/index.js
+++ client/src/components/LanguageSlider/index.js
@@ -1,92 +1,23 @@
 import React from "react";
 import { ListItem } from '../List/ListItem';
-import { sliderSettings } from "./SliderSettings";
+import { sliderSettings, languages } from "./SliderDetails";
+
 import Slider from 'react-slick';
 
 export const LanguageSlider = () => {
+    const languageName = languages.map((name) => {
         return (
-            <Slider {...sliderSettings}>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Known Languages" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="HTML" />
-
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="CSS" />
-                </div>
-                <div>
-
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Javascript" />
-                </div>
-                <div>
-
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Jquery" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Bootstrap" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Node.js" />
-                </div>
-                <div>
-
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Sequelize" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="MySQL" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="GraphQL" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Apollo" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Mongoose" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="Handlebars" />
-                </div>
-                <div>
-
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="EJS" />
-                </div>
-                <div>
-                    <ListItem
-                        className="list-group-item list-group-item-action list-group-item-primary"
-                        name="React" />
-                </div>
-            </Slider>
-
+            <ListItem
+                className="list-group-item list-group-item-action list-group-item-primary"
+                name={name}
+            />
         )
-    }
+    })
+    return (
+        <div id="skills">
+            <Slider {...sliderSettings}>
+                {languageName}
+            </Slider>
+        </div>
+    )
+}
