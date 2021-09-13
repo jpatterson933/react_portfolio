8600365964927e13cda77e5415f94660edfe6943
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-12 18:27:09 -0700

    Added CardDetails.js which I will use to prefil with relatable projects that I am working on that will then be mapped through on the index.js cards component to simplify our coding language

---
+++ client/src/components/Card/CardDetails.js
@@ -0,0 +1,24 @@
+const Cards = [
+    {
+        name: "Weather Dashboard",
+        details: "Find five day forecast for any city in the world. Personalize website by saving city.",
+        summary: ["Weather Reports", "5 Day Forecast", "Save City"],
+        github: "https://github.com/jpatterson933/weather-dashboard",
+        url: "https://jpatterson933.github.io/weather-dashboard/",
+    },
+    {
+        name: "Readme Markdown Generator",
+        details: "Generate Markdown files for any project",
+        summary: ["Easy to use", "Pre-formatted", "Pre-Built"],
+        github: "https://github.com/jpatterson933/readme-generator",
+        url: "https://github.com/jpatterson933/readme-generator",
+    },
+    {
+        name: "Yewnity",
+        details: "A place to go and learn about others and events that you may want to get involved with",
+        summary: ["Live Feed", "Personal Profiles", "Full Stack MVC"],
+        github: "https://github.com/Sharon1106/Yewnity",
+        url: "https://intense-cove-54104.herokuapp.com/",
+    },
+
+]
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -4,8 +4,8 @@ export const Card = (props) => {
     return (
         <div className="card">
             <div className="card-body desrip-holder">
-                <h5 className="card-title">{props.cardTitle}</h5>
-                <p className="card-text">{props.cardDescrip}</p>
+                <h5 className="card-title">{props.cardtitle}</h5>
+                <p className="card-text">{props.carddetails}</p>
             </div>
         </div>
     )
